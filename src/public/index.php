da3c27b68b693ffdff604df67e48d5006335f673
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-03 21:53:29 +0900

    feat: コンタクトフォームの作成

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,33 @@
 <?php
 echo 'Welcome TECH QUEST!';
+<body>
+  
+  <h1>お問い合わせフォーム</h1>
+  <p>以下のフォームからお問い合わせください。</p>
+  
+  <form action="./complete.php" method="post">
+    <table>
+      <tr>
+        <td>タイトル（必須）</td>
+        <td><input name="title"></td>
+      </tr>
+
+      <tr>
+        <td>Email（必須）</td>
+        <td><input name="email"></td>
+      </tr>
+
+      <tr>
+        <td>お問い合わせ内容（必須）</td>
+        <td><textarea name="content"></textarea></td>
+      </tr>
+
+      <tr>
+        <td></td>
+        <td><button type="submit" name="button">送信</button></td>
+      </tr>
+
+    </table>
+  </form>
+
+</body>
